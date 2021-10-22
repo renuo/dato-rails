b2a78962902f3ff54efdc824fbf1c5cfe07079e5
Alessandro Rodi <alessandro.rodi@renuo.ch>
2021-10-22 16:37:02 +0200

    Allow to use preview endpoint

--- lib/dato/client.rb
+++ lib/dato/client.rb
@@ -1,8 +1,8 @@
 module Dato
   class Client < GQLi::Client
-    def initialize(api_token = ENV["DATO_API_TOKEN"], validate_query: true)
+    def initialize(api_token = ENV["DATO_API_TOKEN"], validate_query: true, preview: false)
       super(
-        "https://graphql.datocms.com/",
+        "https://graphql.datocms.com/#{'preview' if preview}",
         headers: {
           "Authorization" => api_token
         },
