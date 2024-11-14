8b41ce0599bc1d12891bceb23b5e6bc833e374d8
Alessandro Rodi <coorasse@gmail.com>
2024-11-14 12:36:10 +0100

    Move fixtures into the spec folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,9 @@ require "support/queries"
 require "vcr"
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.default_cassette_options = {record: :new_episodes}
+  config.default_cassette_options = {record: :once}
   config.configure_rspec_metadata!
   config.before_record do |i|
     i.request.headers.delete("Authorization")
