36ebe996c6bf1dceed424b9088c427d3103954b2
Alessandro Rodi <coorasse@gmail.com>
2024-11-14 14:52:47 +0100

    Correct responsiveImage attributes assignment

--- app/components/dato/responsive_image.rb
+++ app/components/dato/responsive_image.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Dato::ResponsiveImage < Dato::Node
-  def initialize(node, root = nil, custom_css_classes: [])
+  def initialize(node, root = nil, custom_css_classes: [], custom_img_css_classes: [])
     super(node, root)
-    @custom_css_classes = custom_css_classes
+    @custom_css_classes = custom_css_classes.is_a?(Array) ? custom_css_classes.join(" ") : custom_css_classes
+    @custom_img_css_classes = custom_img_css_classes.is_a?(Array) ? custom_img_css_classes.join(" ") : custom_img_css_classes
   end
 end
