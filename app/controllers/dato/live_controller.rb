c04a3e2f51f027ab6ab44523a96fc7434b46f55a
Alessandro Rodi <alessandro.rodi@renuo.ch>
2026-01-27 14:08:14 +0100

    Live controller fix

--- app/controllers/dato/live_controller.rb
+++ app/controllers/dato/live_controller.rb
@@ -2,6 +2,9 @@ module Dato
   class LiveController < ActionController::Base
     def create
       @data = Hashie::Mash.new(JSON.parse(params[:data], symbolize_names: true).dig(:response, :data))
+      # Ensure HTML format is available for ViewComponent template resolution
+      # ViewComponent 4 requires explicit format matching
+      lookup_context.formats |= [:html]
     end
   end
 end
